e701d6daaa6eb8d107e72579b3848b1f9d6f8f05
znakarik <znakarik@icloud.com>
2023-10-22 23:47:20 +0300

    deleted unused class

--- src/games/calculator/CalculatorPostAnswerAware.js
+++
@@ -1,8 +0,0 @@
-import {PostAnswerAware} from "../general/PostAnswerAware.js";
-
-export class CalculatorPostAnswerAware extends PostAnswerAware {
-
-    getNegative = (rightAnswer, wrongAnswer) => {
-        return `'${wrongAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.`
-    }
-}
