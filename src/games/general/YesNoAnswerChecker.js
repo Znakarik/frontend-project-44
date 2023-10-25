008b2a6acaccc73579aec39457aa78a74f97c601
znakarik <znakarik@icloud.com>
2023-10-25 23:10:14 +0300

    fixed tests

--- src/games/general/YesNoAnswerChecker.js
+++ src/games/general/YesNoAnswerChecker.js
@@ -2,7 +2,7 @@ import AnswerChecker from './AnswerChecker.js';
 
 export default class YesNoAnswerChecker extends AnswerChecker {
   isRightAnswer(clientAnswer) {
-    return isRightAnswer(clientAnswer, this.getRightAnswer());
+    return isRightAnswer(clientAnswer, this.getRightAnswer);
   }
 }
 
