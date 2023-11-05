167d7b58c5f59e361ae469856918ab235271cb48
znakarik <znakarik@icloud.com>
2023-11-05 23:04:33 +0300

    refactored brains-gcd game to functional style

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,5 +1,32 @@
 #!/usr/bin/env node
 
-import game from '../src/games/gcd/Game.js';
+import {initGame} from "../src/functional/general/game.js";
+import {generateRandomNonZero} from "../src/functional/general/math.js";
 
-game();
+const MAX_TRY = 3;
+
+function createRightAnswers() {
+    let rightAnswers = new Map();
+
+    for (let i = 0; i < MAX_TRY; i++) {
+        const left = generateRandomNonZero(50);
+        const right = generateRandomNonZero(50);
+        let gcd = findGCD(left, right);
+
+        rightAnswers.set(`${left} ${right}`, gcd);
+    }
+    return rightAnswers;
+}
+
+function findGCD (left, right) {
+    while (right) {
+        const tmp = right;
+        right = left % right;
+        left = tmp;
+    }
+    return left;
+}
+
+
+initGame(createRightAnswers(), 'Find the greatest common divisor of given numbers.');
+
--- src/functional/general/math.js
+++ src/functional/general/math.js
@@ -1,9 +1,17 @@
-
 function generateRandomInt(max) {
     return Math.round(Math.random() * max);
 }
 
+function generateRandomNonZero(max) {
+    let result = 0;
+    while (result === 0) {
+        result = Math.round(Math.random() * max);
+    }
+
+    return result;
+}
+
 
 export {
-    generateRandomInt
+    generateRandomInt, generateRandomNonZero
 }
