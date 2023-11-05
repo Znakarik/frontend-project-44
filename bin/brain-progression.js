7bca71e1eb49d81fbe688e781e21554bc05dc3d4
znakarik <znakarik@icloud.com>
2023-11-05 23:09:46 +0300

    refactored brain-progression to functional style

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,5 +1,37 @@
 #!/usr/bin/env node
 
-import game from '../src/games/progression/Game.js';
+import {initGame} from "../src/functional/general/game.js";
+import {generateRandomInt, generateRandomNonZero} from "../src/functional/general/math.js";
 
-game();
+
+const MAX_TRY = 3;
+const MAX_PROGRESSION_SIZE = 10;
+
+function createRightAnswers() {
+    let rightAnswers = new Map();
+
+    for (let i = 0; i < MAX_TRY; i++) {
+        const step = generateRandomInt(10);
+        const initialNumber = generateRandomInt(10);
+        const missingIndexInProgression = generateRandomInt(MAX_PROGRESSION_SIZE);
+
+        const progression = [];
+
+        progression.push(initialNumber);
+        for (let i = 1; i < MAX_PROGRESSION_SIZE; i++) {
+            progression.push(progression[i - 1] + step);
+        }
+        const rightNumber = progression.at(missingIndexInProgression);
+        const questionToClient = progressionToString(progression, missingIndexInProgression);
+        rightAnswers.set(questionToClient, rightNumber);
+    }
+    return rightAnswers;
+}
+
+function progressionToString(progression, missingIndex) {
+    const copy = [...progression];
+    copy[missingIndex] = '..';
+    return copy.join(' ');
+}
+
+initGame(createRightAnswers(), 'What number is missing in the progression?');
