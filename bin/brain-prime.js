a7e8497e875e87acd9c73643bd0f1c1b69c343b7
znakarik <znakarik@icloud.com>
2024-05-29 20:02:44 +0300

    deleted business logic in brain-prime.js after review

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,23 +1,5 @@
 #!/usr/bin/env node
 
-import initGame from '../src/functional/general/game.js';
-import { getRandomNumber } from '../src/functional/general/primeNumbersAware.js';
+import startGame from "../src/functional/game/brainPrime.js";
 
-const MAX_TRY = 3;
-
-function createRightAnswers() {
-  const rightAnswers = new Map();
-  let i = 0;
-
-  while (i < MAX_TRY) {
-    const rightAnswer = getRandomNumber();
-    rightAnswers.set(rightAnswer, 'yes');
-    i += 1;
-  }
-  return rightAnswers;
-}
-
-initGame(
-  createRightAnswers(),
-  'Answer "yes" if given number is prime. Otherwise answer "no".',
-);
+startGame();
