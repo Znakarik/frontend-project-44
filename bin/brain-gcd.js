c1b4b45d0df75df0dc27358ae80c1e8495ee110a
znakarik <znakarik@icloud.com>
2024-05-29 20:01:30 +0300

    deleted business logic in brain-gcd.js after review

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,36 +1,5 @@
 #!/usr/bin/env node
 
-import initGame from '../src/functional/general/game.js';
-import { generateRandomNonZero } from '../src/functional/general/math.js';
+import startGame from "../src/functional/game/brainGcd.js";
 
-const MAX_TRY = 3;
-
-function findGCD(left, right) {
-  let upper = left;
-  let divider = right;
-  while (divider) {
-    const tmp = divider;
-    divider = upper % divider;
-    upper = tmp;
-  }
-  return upper;
-}
-
-function createRightAnswers() {
-  const rightAnswers = new Map();
-  let i = 0;
-  while (i < MAX_TRY) {
-    const left = generateRandomNonZero(50);
-    const right = generateRandomNonZero(50);
-    const gcd = findGCD(left, right);
-
-    rightAnswers.set(`${left} ${right}`, gcd);
-    i += 1;
-  }
-  return rightAnswers;
-}
-
-initGame(
-  createRightAnswers(),
-  'Find the greatest common divisor of given numbers.',
-);
+startGame();
