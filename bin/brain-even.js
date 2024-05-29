649c82151f9f7ffe82a7910a71bae6f2f3b0f0cf
znakarik <znakarik@icloud.com>
2024-05-29 19:48:13 +0300

    deleted business logic in brain-even.js after review comments

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,20 +1,5 @@
 #!/usr/bin/env node
 
-import initGame from '../src/functional/general/game.js';
-import { generateRandomInt } from '../src/functional/general/math.js';
+import {startGame} from "../src/functional/game/brainEvenGame.js";
 
-const MAX_TRY = 3;
-
-function createRightAnswers() {
-  const rightAnswers = new Map();
-  let i = 0;
-  while (i < MAX_TRY) {
-    const number = generateRandomInt(100);
-    rightAnswers.set(number, number % 2 === 0 ? 'yes' : 'no');
-    i += 1;
-  }
-  return rightAnswers;
-}
-
-const rightAnswers = createRightAnswers();
-initGame(rightAnswers, 'Answer "yes" if the number is even, otherwise answer "no".');
+startGame();
---
+++ src/functional/game/brainEvenGame.js
@@ -0,0 +1,23 @@
+import initGame from '../general/game.js';
+import { generateRandomInt } from '../general/math.js';
+
+const MAX_TRY = 3;
+
+function createRightAnswers() {
+    const rightAnswers = new Map();
+    let i = 0;
+    while (i < MAX_TRY) {
+        const number = generateRandomInt(100);
+        rightAnswers.set(number, number % 2 === 0 ? 'yes' : 'no');
+        i += 1;
+    }
+    return rightAnswers;
+}
+
+const rightAnswers = createRightAnswers();
+
+function startGame() {
+    initGame(rightAnswers, 'Answer "yes" if the number is even, otherwise answer "no".');
+}
+
+export {startGame};
